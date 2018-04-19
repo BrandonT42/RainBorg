dcc463386d4513421c0146b0add915101c6735bc
BrandonT42 <37456193+BrandonT42@users.noreply.github.com>
2018-04-19 16:58:47 -0700

    Update Constants.cs

--- Master/Constants.cs
+++ Master/Constants.cs
@@ -1,1 +1,14 @@
+namespace RainBorg
+{
+    class Constants
+    {
+        // Files
+        public const string StatSheet = "Stats.json";
+        public const string Config = "Config.conf";
 
+        // Bot Values
+        public const string BotToken = "";
+        public const string BalanceURL = "";
+        public const string BotPrefix = "$";
+    }
+}
