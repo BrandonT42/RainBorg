c2fde8d125be1d16960c30dc1b039489f31cfedc
BrandonT42 <37456193+BrandonT42@users.noreply.github.com>
2018-04-19 16:57:49 -0700

    Update SpamFilter.cs

--- Master/SpamFilter.cs
+++ Master/SpamFilter.cs
@@ -1,1 +1,23 @@
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
+namespace RainBorg
+{
+    partial class RainBorg
+    {
+        // Checks a message for spam
+        private Task CheckForSpamAsync(SocketUserMessage message, out bool result)
+        {
+        
+            // Purposely left empty to keep spam filters private
+
+            // Completed
+            result = false;
+            return Task.CompletedTask;
+        }
+    }
+}
