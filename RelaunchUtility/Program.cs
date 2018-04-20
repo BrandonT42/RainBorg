4b058394dad89feb31c389d1c14f6723f529b015
BrandonT42 <37456193+BrandonT42@users.noreply.github.com>
2018-04-19 21:01:49 -0700

    Add files via upload

---
+++ RelaunchUtility/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace RelaunchUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sleeping 5 seconds.");
+            Thread.Sleep(5000);
+            Console.WriteLine("Relaunching application");
+            Process.Start(args[0]);
+        }
+    }
+}
